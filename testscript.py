3808f88aa1aa96fefd93be1f2b33bd5ab2560c53
Kitton._ <tonkittipod@gmail.com>
2021-12-27 00:25:23 +0700

    ☄️

--- testscript.py
+++ testscript.py
@@ -1,12 +1,20 @@
-#   ---**---    -******-                 
-#   --****--    ********        
-#   -******-    ********        
-#   ********    ********        
-#   ********    ********        
-#   -******-    ********        
-#   --****--    ********        
-#   ---**---    -******-         
-n = 8;r = 3;n= 2
-for i in range(1,n+1):
-    print('{}{}{}'.format('_'*(r-i),'*'*(n-(r*i)),'_'*(r-i)))
-    
+
+n = int(input())
+num = [];ans=[];s = 1
+
+for i in range(2,n+2):
+    if n % i == 0:
+        num.append(i)
+for i in num:
+    t = True
+    for j in range(2,i):
+        if i % j == 0:
+            t = False
+    if t :
+        ans.append(i)
+        s *= i
+if s != n:
+    ans.append(n//s)
+ans.sort()
+for i in ans:
+    print(i)
