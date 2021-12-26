ff5c6a0e554fdb5bf71e58e9fe99902feafeb2df
Kitton._ <tonkittipod@gmail.com>
2021-12-26 09:10:00 +0700

    .

--- testscript.py
+++ testscript.py
@@ -1,1 +1,16 @@
-print([1,5] ==[1,6])
+#  *
+# ***
+#  *
+# ***
+#*****
+#  |
+#==V==
+
+n = 2
+for k in range(2,n+2):
+    for i in range(1,k+1):
+        for j in range(k-i):
+            print(' ',end='')
+        for m in range(i*2-1):
+            print('*',end='')
+        print()
