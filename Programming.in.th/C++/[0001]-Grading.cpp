9a5c14247910405958d7cd0daab19aef74f6f3ab
Kitton._ <tonkittipod@gmail.com>
2022-01-11 09:36:14 +0700

    .

--- Programming.in.th/C++/[0001]-Grading.cpp
+++ Programming.in.th/C++/[0001]-Grading.cpp
@@ -2,41 +2,26 @@
 using namespace std;
 
 int main() {
-  int hw,mid,final,tot;
-  cin >> hw >> mid >> final;
-  tot = hw + mid + final;
-  if (tot >= 80 && tot <= 100)
-  {
-    cout << "A";
-  }
-  else if (tot >= 75 && tot <= 79)
-  {
-    cout << "B+";
-  }
-  else if (tot >= 70 && tot <= 74)
-  {
-    cout << "B";
-  }
-  else if (tot >= 65 && tot <= 69)
-  {
-    cout << "C+";
-  }
-  else if (tot >= 60 && tot <= 64)
-  {
-    cout << "C";
-  }
-  else if (tot >= 55 && tot <= 59)
-  {
-    cout << "D+";
-  }
-  else if (tot >= 50 && tot <= 54)
-  {
-    cout << "D";
-  }
-  else
-  {
-    cout << "F";
-  }
-  
-  return 0;
+    int a,b,c,t;
+    cin >> a >> b >> c;
+    t = a + b + c;
+    if (t >= 80 && t <= 100){
+        cout << "A";
+    } else if (t >= 75 && t <= 79) {
+        cout << "B+";
+    } else if (t >= 70 && t <= 74) {
+        cout << "B";
+    } else if (t >= 65 && t<= 69) {
+        cout << "C+";
+    } else if (t >= 60 && t<= 64) {
+        cout << "C";
+    } else if (t >= 55 && t<=59) {
+        cout << "D+";
+    } else if (t >= 50 && t <= 54) {
+        cout << "D";
+    } else {
+        cout << "F";
+    }
+    
+    return 0;
 }
--- Programming.in.th/C++/[0002]-Min Max.cpp
+++ Programming.in.th/C++/[0002]-Min Max.cpp
@@ -2,19 +2,18 @@
 using namespace std;
 
 int main() {
-    int nums,M= -2000000000,m = 2000000000,num;
-    cin >> nums;
-    for (int i = 0; i < nums; i++)
-    {
-        cin >> num;
-        if (num > M) {
-            M = num;
-        } 
-        if (num < m) {
-            m = num;
+    int M = -2000000000, m = 2000000000,n;
+    cin >> n;
+    for (int i = 0; i < n; i++){
+        int x;
+        cin >> x;
+        if (x > M) {
+            M = x;
+        }
+        if (x < m) {
+            m = x;
         }
     }
     cout << m << endl << M;
-
     return 0;
 }
