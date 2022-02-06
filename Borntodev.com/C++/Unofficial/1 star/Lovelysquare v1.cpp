391bc45263f06cb64e0f8d583099399c156f981c
Kitton._ <tonkittipod@gmail.com>
2022-02-07 00:17:05 +0700

    C++

---
+++ Borntodev.com/C++/Unofficial/1 star/Lovelysquare v1.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include <math.h>
+using namespace std;
+
+int main(){
+    double d;
+    cin >> d;
+    cout << setprecision(2) << fixed;
+    cout << sqrt(pow(d,2)/2)*4 << endl;
+    cout << pow(d,2)/2;
+    
+    return 0;
+}
