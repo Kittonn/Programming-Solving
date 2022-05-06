a3bd2d3759c931c61f028b5bf41f310ebc8b59f4
Kitton._ <tonkittipod@gmail.com>
2022-05-06 20:26:41 +0700

    FCC

---
+++ FreeCodeCamp/Rosetta Code/FizzBuzz.ts
@@ -0,0 +1,10 @@
+function fizzBuzz() {
+  let answer = [];
+  for (let i = 1; i < 101; i++) {
+    if (i % 5 === 0 && i % 3 === 0) answer.push("FizzBuzz");
+    else if (i % 3 === 0) answer.push("Fizz");
+    else if (i % 5 === 0) answer.push("Buzz");
+    else answer.push(i);
+  }
+  return answer;
+}
